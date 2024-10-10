cc74173dc5a460e7354cff9210a821ccc23ff897
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-10-10 15:01:48 -0500

    refactor: :dizzy: tweak list animation

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View, FlatList } from "react-native";
-import { useRouter } from "expo-router";
 import SafeAreaContent from "@/components/SafeAreaContent";
 import { useTaskActionsQuery } from "@/lib/queries";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+import { AnimatePresence, View as MotiView } from "moti";
 
 const App = () => {
   const router = useRouter();
@@ -22,17 +23,25 @@ const App = () => {
       {isLoading && <Text>Loading task actions...</Text>}
       {error && <Text>Error loading task actions: {error.message}</Text>}
       {data && (
-        <FlatList
-          data={data.task_action}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View className="mt-4">
-              <Text className="font-bold">{item.Task_name}</Text>
-              <Text>Type: {item.Task_type}</Text>
-              <Text>Role: {item.task_role}</Text>
-            </View>
-          )}
-        />
+        <AnimatePresence>
+          <FlatList
+            data={data.task_action}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item, index }) => (
+              <MotiView
+                from={{ opacity: 0, translateX: -10 }}
+                animate={{ translateX: 0, opacity: 1 }}
+                exit={{ opacity: 0, translateX: -10 }}
+                transition={{ delay: index * 60, type: "spring" }}
+                className="mt-4 text-base"
+              >
+                <Text className="font-bold text-lg">{item.Task_name}</Text>
+                <Text>Type: {item.Task_type}</Text>
+                <Text>Role: {item.task_role}</Text>
+              </MotiView>
+            )}
+          />
+        </AnimatePresence>
       )}
 
       <StatusBar style="light" />
