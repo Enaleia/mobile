83eb30c351699f4c916ac4fecd3eca5aced498cd
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-10-10 13:49:14 -0500

    chore: :fire: Remove unused files

---
+++ components/SafeAreaContent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaContent = ({ children }: { children: any }) => {
+  return <SafeAreaView className="flex-1 p-5">{children}</SafeAreaView>;
+};
+
+export default SafeAreaContent;
