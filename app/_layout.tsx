d9c9a069ea3092a26eec98b2a3ca5e1c2cebdfb4
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-10-30 12:48:21 -0500

    feat(cache): :sparkles: Add local first and data persistence to tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,40 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import NetInfo from "@react-native-community/netinfo";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { onlineManager, QueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { Stack } from "expo-router";
-import "./onlineManager";
+import { useEffect } from "react";
+
+const queryClient = new QueryClient();
+
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+  throttleTime: 3000,
+});
 
 export default function RootLayout() {
-  const queryClient = new QueryClient();
+  useEffect(() => {
+    return NetInfo.addEventListener((state) => {
+      const status = !!state.isConnected;
+      onlineManager.setOnline(status);
+    });
+  }, []);
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      onSuccess={() =>
+        queryClient
+          .resumePausedMutations()
+          .then(() => queryClient.invalidateQueries())
+      }
+      persistOptions={{ persister }}
+      client={queryClient}
+    >
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 }
