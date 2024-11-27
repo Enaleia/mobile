677ed8fa61263eb3c3865f62149261603b9181bd
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-11-27 14:49:19 -0500

    feat(activity): :construction: Add activity details page
    Add link to activity details from card (bug: only works for complete items)

--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -1,14 +1,27 @@
 import Chip from "@/components/generic/Chip";
 import actionIcons from "@/constants/ActionIcons";
 import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import { MotiView } from "moti";
-import { Image, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 
+/**
+ * Represents an activity in the UI
+ * @interface UIActivity
+ * @property {string} date - The date of the activity in ISO format (YYYY-MM-DD)
+ * @property {"Pending" | "In Progress" | "Complete"} status - The current status of the activity
+ * @property {string} location - The location where the activity takes place
+ * @property {string} title - The name/title of the activity
+ * @property {string} id - Unique identifier for the activity
+ * @property {boolean} [asLink] - Optional flag to indicate if the activity should be rendered as a link
+ */
 export interface UIActivity {
   date: string;
   status: "Pending" | "In Progress" | "Complete";
   location: string;
   title: string;
+  id: string;
+  asLink?: boolean;
 }
 
 /**
@@ -20,33 +33,42 @@ function AnimatedActivityCardList({
 }: {
   activities: UIActivity[];
 }) {
-  return activities.map((activity, index) => (
-    <MotiView
-      key={activity.status + index}
-      className="mb-2 last-of-type:mb-0"
-      from={{
-        opacity: 0,
-        translateY: 20,
-      }}
-      animate={{
-        opacity: 1,
-        translateY: 0,
-      }}
-      // Stagger the animation by delaying each card
-      transition={{
-        type: "timing",
-        duration: 500,
-        delay: index * 100, // 100ms delay between each card
-      }}
-    >
-      <ActivityCard
-        title={activity.title}
-        date={activity.date}
-        status={activity.status}
-        location={activity.location}
-      />
-    </MotiView>
-  ));
+  return activities.map((activity, index) => {
+    console.log(
+      `Rendering activity: ${activity.title}, asLink: ${
+        activity.asLink
+      }, link: ${activity.asLink && `/activities/${activity.id}`}`
+    );
+    return (
+      <MotiView
+        key={activity.status + index}
+        className="mb-2 last-of-type:mb-0"
+        from={{
+          opacity: 0,
+          translateY: 20,
+        }}
+        animate={{
+          opacity: 1,
+          translateY: 0,
+        }}
+        // Stagger the animation by delaying each card
+        transition={{
+          type: "timing",
+          duration: 500,
+          delay: index * 100, // 100ms delay between each card
+        }}
+      >
+        <ActivityCard
+          id={activity.id}
+          title={activity.title}
+          date={activity.date}
+          status={activity.status}
+          location={activity.location}
+          asLink={activity.asLink}
+        />
+      </MotiView>
+    );
+  });
 }
 
 const activityTitleIcons = {
@@ -64,20 +86,53 @@ const activityTitleIcons = {
 
 /**
  * A card component that displays an activity with a date, status, location, and title
+ * @param id - The id of the activity
  * @param date - The date of the activity
  * @param status - The status of the activity
  * @param location - The location of the activity
  * @param title - The title of the activity
+ * @param asLink - Whether the activity is a link, defaults to true
  */
-function ActivityCard({ title, date, status, location }: UIActivity) {
-  const statusClasses = {
+function ActivityCard({
+  id,
+  title,
+  date,
+  status,
+  location,
+  asLink = true,
+}: UIActivity) {
+  console.log(`Activity ${title} (${status}): asLink = ${asLink}`);
+  const chipClassesByStatus = {
     Pending: "bg-neutral-100 border-neutral-300",
     "In Progress": "bg-yellow-300 border-yellow-500",
     Complete: "bg-green-600 border-green-600",
   } as const;
 
+  const Wrapper = ({
+    children,
+    pressFn,
+    classes,
+  }: {
+    children: React.ReactNode;
+    pressFn?: () => void;
+    classes?: string;
+  }) => {
+    if (asLink) {
+      console.log(`Wrapper: Pressable`);
+      return (
+        <Pressable className={classes} onPress={pressFn} key={id}>
+          {children}
+        </Pressable>
+      );
+    }
+    return <View className={classes}>{children}</View>;
+  };
+
   return (
-    <View className="p-3 bg-neutral-50 rounded-lg border-[1.5px] border-neutral-200">
+    <Wrapper
+      classes="p-3 bg-neutral-50 rounded-lg border-[1.5px] border-neutral-200 active:bg-neutral-100 active:scale-95 transition-all duration-150"
+      pressFn={() => asLink && router.push(`/activities/${id}`)}
+    >
       <View className="flex-row">
         <View className="flex-1 flex-col space-y-1.5">
           <Text className="font-dm-medium text-lg tracking-tighter leading-[22px]">
@@ -113,7 +168,9 @@ function ActivityCard({ title, date, status, location }: UIActivity) {
                 ? "text-yellow-800"
                 : "text-white"
             }
-            extraClassName={statusClasses[status as keyof typeof statusClasses]}
+            extraClassName={
+              chipClassesByStatus[status as keyof typeof chipClassesByStatus]
+            }
           />
         </View>
         <Chip
@@ -121,7 +178,7 @@ function ActivityCard({ title, date, status, location }: UIActivity) {
           icon={<Ionicons name="location" size={16} color="#24548b" />}
         />
       </View>
-    </View>
+    </Wrapper>
   );
 }
 
