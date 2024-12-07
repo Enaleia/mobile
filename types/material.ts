b174f6cb7029e569d6b7c6c57a5e6a36cb97200d
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-07 15:08:08 -0500

    feat(materials): :label: Update material constants and types
    Add material ids and option values

---
+++ types/material.ts
@@ -0,0 +1,27 @@
+export type MaterialNames =
+  | "Mixed Plastics"
+  | "Metals"
+  | "Prevention Nets"
+  | "Ghost Nets"
+  | "Others"
+  | "PP"
+  | "HDPE"
+  | "LDPE"
+  | "PET"
+  | "Nets"
+  | "Non-Recyclables"
+  | "PS"
+  | "Rubbers"
+  | "PA"
+  | "PE"
+  | "Ropes"
+  | "Mixed Materials";
+
+export type MaterialOption = {
+  label: MaterialNames;
+  value: number;
+};
+
+export type MaterialOptions = MaterialOption[];
+
+export type MaterialIdMap = Record<MaterialNames, number>;
