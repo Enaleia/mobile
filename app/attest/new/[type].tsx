6ac46cea0c77b68007925a3b2fe39b9851b682f8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-08 18:54:54 -0500

    refactor(attest): :sparkles: Update UI for incoming and outgoing materials

--- app/attest/new/[type].tsx
+++ app/attest/new/[type].tsx
@@ -1,15 +1,14 @@
 import ActionButton from "@/components/ActionButton";
-import MaterialPickerModal from "@/components/MaterialPickerModal";
 import SafeAreaContent from "@/components/SafeAreaContent";
 import AddMaterialModal from "@/components/forms/AddMaterialModal";
-import MaterialsSelect from "@/components/forms/MaterialsSelect";
-import QRTextInput from "@/components/forms/QRTextInput";
 import { ACTION_SLUGS } from "@/constants/action";
+import { MATERIAL_ID_TO_NAME } from "@/constants/material";
 import { ActionTitle } from "@/types/action";
+import { MaterialDetails } from "@/types/material";
 import { Ionicons } from "@expo/vector-icons";
 import { Link, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 
 export default function NewActionScreen() {
   const { type } = useLocalSearchParams(); // slug format
@@ -18,23 +17,20 @@ export default function NewActionScreen() {
     (key) => ACTION_SLUGS[key as ActionTitle] === type
   ) as ActionTitle;
 
-  const [selectedMaterials, setSelectedMaterials] = useState<number[]>([]);
-  const [selectedMaterialDetails, setSelectedMaterialDetails] = useState<{
-    materialId: number;
-    weight: number;
-  } | null>(null);
+  const [selectedIncomingMaterialDetails, setSelectedIncomingMaterialDetails] =
+    useState<MaterialDetails>({});
 
-  const handleUpdateMaterialDetails = (
-    materialId: number,
-    weight: number = 0
-  ) => {
-    setSelectedMaterials((prev) => [...prev, materialId]);
-    setSelectedMaterialDetails({ materialId, weight });
-  };
+  const [selectedOutgoingMaterialDetails, setSelectedOutgoingMaterialDetails] =
+    useState<MaterialDetails>({});
 
   const [
     isIncomingMaterialsPickerVisible,
     setIsIncomingMaterialsPickerVisible,
+  ] = useState(false);
+
+  const [
+    isOutgoingMaterialsPickerVisible,
+    setIsOutgoingMaterialsPickerVisible,
   ] = useState(false);
 
   return (
@@ -46,56 +42,193 @@ export default function NewActionScreen() {
         </Pressable>
       </Link>
       <ActionButton title={title} presentation="banner" />
-
-      <View className="mt-6">
-        <Text className="text-lg font-dm-medium text-neutral-600 tracking-tighter mb-2">
-          Incoming materials
-        </Text>
-        <View className="bg-neutral-50 p-1">
-          <Text className="text-sm font-dm-medium text-neutral-600 mb-1">
-            Mixed Materials
-          </Text>
-          <View className="flex-row gap-1">
-            <View className="flex-1 min-w-[100px] h-40">
-              <QRTextInput value="" onChangeText={() => {}} />
-            </View>
-            <View className="min-w-[100px]">
-              <TextInput
-                inputMode="numeric"
-                placeholder="Weight in kg"
-                className="border-[1.5px] border-neutral-300 rounded-md"
-              />
-            </View>
-          </View>
-        </View>
-        <AddMaterialModal
-          isVisible={isIncomingMaterialsPickerVisible}
-          onClose={() => setIsIncomingMaterialsPickerVisible(false)}
-          selectedMaterials={selectedMaterials}
-          setSelectedMaterials={setSelectedMaterials}
-          type="incoming"
-        />
-        <Pressable
-          className="flex-row items-center justify-center mt-2 bg-slate-300 p-3 rounded-md"
-          onPress={() => setIsIncomingMaterialsPickerVisible(true)}
+      <View className="flex-1 py-1 shadow-inner">
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ flexGrow: 0, paddingBottom: 20 }}
         >
-          <Text className="text-sm font-dm-medium text-neutral-600">
-            {selectedMaterials?.length > 0
-              ? "Update materials"
-              : "Add materials"}
-          </Text>
-        </Pressable>
-      </View>
-
-      <View className="mt-6">
-        <Text className="text-lg font-dm-medium text-neutral-600 tracking-tighter">
-          Outgoing materials
-        </Text>
-        <View className="flex-row flex-wrap px-0.5 bg-slate-500">
-          <View className="w-[48%] mr-1 mb-1 active:opacity-70 transition-opacity duration-200 active:scale-95 ease-out">
-            <Text>Material 1</Text>
+          <View className="mt-6">
+            <View className="flex-row items-center justify-between px-1">
+              <Text className="text-base font-dm-medium text-slate-600 tracking-tighter mb-2">
+                Incoming materials
+              </Text>
+              <Text className="text-sm font-dm-medium text-slate-600 tracking-tighter bg-slate-200 px-1 py-0.5 rounded-full min-w-6 min-h-6 flex-row items-center justify-center">
+                {Object.keys(selectedIncomingMaterialDetails).length}
+              </Text>
+            </View>
+            <View className="flex-col items-center justify-center max-h-[300px]">
+              {selectedIncomingMaterialDetails ? (
+                <ScrollView className="w-full rounded-lg overflow-clip border-[1.5px] border-slate-200">
+                  {Object.entries(selectedIncomingMaterialDetails).map(
+                    ([materialId, { weight, code }]) => (
+                      <View
+                        key={materialId}
+                        className="border-b-[1.5px] last-of-type:border-b-0 border-slate-200"
+                      >
+                        <View className="bg-slate-50 w-full">
+                          <Text className="text-base font-dm-bold tracking-tight text-slate-950 bg-slate-50 px-3 py-2">
+                            {MATERIAL_ID_TO_NAME[materialId]}
+                          </Text>
+                          <View className="flex-row justify-between px-3 pb-2">
+                            {weight && (
+                              <View className="flex-row items-center justify-center space-x-1">
+                                <View className="bg-slate-200 rounded-full p-1 h-6 w-6 flex-row items-center justify-center">
+                                  <Ionicons
+                                    name="scale-outline"
+                                    size={14}
+                                    color="#24548b"
+                                  />
+                                </View>
+                                <View className="flex-row items-baseline gap-1">
+                                  <Text className="text-sm font-dm-medium text-slate-500">
+                                    {weight}
+                                  </Text>
+                                  <Text className="text-xs font-dm-medium text-slate-500">
+                                    kg
+                                  </Text>
+                                </View>
+                              </View>
+                            )}
+                            {code && (
+                              <View className="flex-row items-center justify-center gap-1">
+                                <View className="bg-slate-200 rounded-full p-1 h-6 w-6 flex-row items-center justify-center">
+                                  <Ionicons
+                                    name="barcode-outline"
+                                    size={14}
+                                    color="#24548b"
+                                  />
+                                </View>
+                                <Text className="font-mono text-sm text-slate-500 bg-slate-200 px-1 py-0.5 rounded-md">
+                                  {code}
+                                </Text>
+                              </View>
+                            )}
+                          </View>
+                        </View>
+                        <View className="py-1 bg-slate-50" />
+                      </View>
+                    )
+                  )}
+                </ScrollView>
+              ) : (
+                <View className="bg-slate-50 rounded-lg p-2">
+                  <Text className="text-sm font-dm-medium text-neutral-600">
+                    No materials selected
+                  </Text>
+                </View>
+              )}
+            </View>
+            <AddMaterialModal
+              isVisible={isIncomingMaterialsPickerVisible}
+              onClose={() => setIsIncomingMaterialsPickerVisible(false)}
+              selectedMaterials={selectedIncomingMaterialDetails}
+              setSelectedMaterials={setSelectedIncomingMaterialDetails}
+              type="incoming"
+            />
+            <Pressable
+              className="flex-row items-center justify-center mt-2 bg-slate-200 px-3 py-2 rounded-md"
+              onPress={() => setIsIncomingMaterialsPickerVisible(true)}
+            >
+              <Text className="text-base font-dm-medium text-slate-700 tracking-tight">
+                {Object.keys(selectedIncomingMaterialDetails).length > 0
+                  ? "Update incoming materials"
+                  : "Add incoming materials"}
+              </Text>
+            </Pressable>
           </View>
-        </View>
+
+          <View className="h-px bg-slate-300 my-8" />
+
+          <View className="">
+            <View className="flex-row items-center justify-between px-1">
+              <Text className="text-base font-dm-medium text-slate-600 tracking-tighter mb-2">
+                Outgoing materials
+              </Text>
+              <Text className="text-sm font-dm-medium text-slate-600 tracking-tighter bg-slate-200 px-1 py-0.5 rounded-full min-w-6 min-h-6 flex-row items-center justify-center">
+                {Object.keys(selectedOutgoingMaterialDetails).length}
+              </Text>
+            </View>
+            <View className="flex-col items-center justify-center max-h-[300px]">
+              {selectedOutgoingMaterialDetails ? (
+                <ScrollView className="w-full rounded-lg overflow-clip border-[1.5px] border-slate-200">
+                  {Object.entries(selectedOutgoingMaterialDetails).map(
+                    ([materialId, { weight, code }]) => (
+                      <View
+                        key={materialId}
+                        className="border-b-[1.5px] last-of-type:border-b-0 border-slate-200"
+                      >
+                        <View className="bg-slate-50 w-full">
+                          <Text className="text-base font-dm-bold tracking-tight text-slate-950 bg-slate-50 px-3 py-2">
+                            {MATERIAL_ID_TO_NAME[materialId]}
+                          </Text>
+                          <View className="flex-row justify-between px-3 pb-2">
+                            {weight && (
+                              <View className="flex-row items-center justify-center space-x-1">
+                                <View className="bg-slate-200 rounded-full p-1 h-6 w-6 flex-row items-center justify-center">
+                                  <Ionicons
+                                    name="scale-outline"
+                                    size={14}
+                                    color="#24548b"
+                                  />
+                                </View>
+                                <View className="flex-row items-baseline gap-1">
+                                  <Text className="text-sm font-dm-medium text-slate-500">
+                                    {weight}
+                                  </Text>
+                                  <Text className="text-xs font-dm-medium text-slate-500">
+                                    kg
+                                  </Text>
+                                </View>
+                              </View>
+                            )}
+                            {code && (
+                              <View className="flex-row items-center justify-center gap-1">
+                                <View className="bg-slate-200 rounded-full p-1 h-6 w-6 flex-row items-center justify-center">
+                                  <Ionicons
+                                    name="barcode-outline"
+                                    size={14}
+                                    color="#24548b"
+                                  />
+                                </View>
+                                <Text className="font-mono text-sm text-slate-500 bg-slate-200 px-1 py-0.5 rounded-md">
+                                  {code}
+                                </Text>
+                              </View>
+                            )}
+                          </View>
+                        </View>
+                        <View className="py-1 bg-slate-50" />
+                      </View>
+                    )
+                  )}
+                </ScrollView>
+              ) : (
+                <View className="bg-slate-50 rounded-lg p-2">
+                  <Text className="text-sm font-dm-medium text-neutral-600">
+                    No materials selected
+                  </Text>
+                </View>
+              )}
+            </View>
+            <AddMaterialModal
+              isVisible={isOutgoingMaterialsPickerVisible}
+              onClose={() => setIsOutgoingMaterialsPickerVisible(false)}
+              selectedMaterials={selectedOutgoingMaterialDetails}
+              setSelectedMaterials={setSelectedOutgoingMaterialDetails}
+              type="outgoing"
+            />
+            <Pressable
+              className="flex-row items-center justify-center mt-2 bg-slate-200 px-3 py-2 rounded-md"
+              onPress={() => setIsOutgoingMaterialsPickerVisible(true)}
+            >
+              <Text className="text-base font-dm-medium text-slate-700 tracking-tight">
+                {Object.keys(selectedOutgoingMaterialDetails).length > 0
+                  ? "Update outgoing materials"
+                  : "Add outgoing materials"}
+              </Text>
+            </Pressable>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaContent>
   );
