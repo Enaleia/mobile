c9df5c2d0e400429cb84389cb46db30f423d5954
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-16 16:13:47 -0500

    feat(queue): :lipstick: Update UI, data fetching and sorting for queue page
    Sort queue by most recent, get queued items from local storage, tweak UI styles

--- app/(tabs)/queue.tsx
+++ app/(tabs)/queue.tsx
@@ -1,76 +1,33 @@
+import { IEvent } from "@/api/events/new";
 import SafeAreaContent from "@/components/SafeAreaContent";
-import { ACTION_COLORS, ACTION_ICONS } from "@/constants/action";
+import { ACTION_ICONS } from "@/constants/action";
 import { ActionTitle } from "@/types/action";
 import { Ionicons } from "@expo/vector-icons";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigation } from "expo-router";
 import { useEffect } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
-type QueueStatus = "Awaiting network" | "Uploading" | "Completed";
-
-const QUEUE_ACTIONS: {
-  title: ActionTitle;
-  date: string;
-  status: QueueStatus;
-}[] = [
-  { title: "Fishing for litter", date: "Dec 15, 2024", status: "Uploading" },
-  { title: "Sorting", date: "Jan 3, 2025", status: "Awaiting network" },
-  { title: "Beach Cleanup", date: "Feb 12, 2025", status: "Completed" },
-  { title: "Pelletizing", date: "Nov 28, 2024", status: "Awaiting network" },
-  { title: "Washing", date: "Dec 1, 2024", status: "Completed" },
-  { title: "Prevention", date: "Jan 20, 2025", status: "Uploading" },
-  { title: "Manufacturing", date: "Nov 10, 2024", status: "Awaiting network" },
-];
-
-const QUEUE_STATUS_CATEGORIES: Record<QueueStatus, typeof QUEUE_ACTIONS> = {
-  "Awaiting network": QUEUE_ACTIONS.filter(
-    (action) => action.status === "Awaiting network"
-  ),
-  Uploading: QUEUE_ACTIONS.filter((action) => action.status === "Uploading"),
-  Completed: QUEUE_ACTIONS.filter((action) => action.status === "Completed"),
-};
-
-export const INCOMPLETE_ACTIONS = QUEUE_ACTIONS.filter(
-  (action) => action.status !== "Completed"
-);
-
-const QUEUE_ACTION_STATUS_COLORS = {
-  "Awaiting network": "bg-amber-200 text-amber-600 border-amber-300",
-  Uploading: "bg-blue-200 text-blue-600 border-blue-300",
-  Completed: "bg-green-200 text-green-600 border-green-300",
-};
-
-const QueuedAction = ({
-  title,
-  date,
-  status,
-}: {
-  title: ActionTitle;
-  date: string;
-  status: QueueStatus;
-}) => {
+const QueuedAction = ({ type, date }: { type: ActionTitle; date: string }) => {
   return (
     <View
       className="flex-row items-center justify-between px-3 py-2 border-b border-neutral-200"
-      style={{ borderLeftWidth: 4, borderLeftColor: ACTION_COLORS[title] }}
+      style={{ borderLeftWidth: 4 }}
     >
       <View className="flex-row items-center justify-center gap-2">
-        <Image source={ACTION_ICONS[title]} className="w-8 h-8" />
+        <Image source={ACTION_ICONS[type]} className="w-8 h-8" />
         <View className="space-y-0.5">
           <Text className="text-base font-dm-bold text-neutral-800 tracking-tighter">
-            {title}
+            {type}
           </Text>
           <Text className="text-xs font-dm-regular text-neutral-600 uppercase w-full">
-            {date}
+            {new Intl.DateTimeFormat("en-US", {
+              month: "short",
+              day: "numeric",
+              year: "numeric",
+            }).format(new Date(date))}
           </Text>
         </View>
-      </View>
-      <View className="flex-row items-center justify-center gap-1">
-        <Text
-          className={`text-xs font-dm-medium px-2 py-0.5 rounded-md text-center border-[1.5px] ${QUEUE_ACTION_STATUS_COLORS[status]}`}
-        >
-          {status}
-        </Text>
       </View>
     </View>
   );
@@ -78,17 +35,26 @@ const QueuedAction = ({
 
 const QueueScreen = () => {
   const navigation = useNavigation();
+  const queryClient = useQueryClient();
+
+  const { data: incompleteActions } = useQuery({
+    queryKey: ["events"],
+    queryFn: () =>
+      queryClient
+        .getQueryData<IEvent[]>(["events"])
+        ?.filter((event) => event.isNotSynced) || [],
+    staleTime: Infinity,
+  });
 
   useEffect(() => {
     // Update the tab bar badge with number of incomplete actions
     navigation.setOptions({
-      tabBarBadge:
-        INCOMPLETE_ACTIONS.length > 0 ? INCOMPLETE_ACTIONS.length : undefined,
+      tabBarBadge: incompleteActions?.length || undefined,
     });
-  }, [INCOMPLETE_ACTIONS]);
+  }, [incompleteActions]);
 
-  const QUEUE_ACTIONS_SORTED = Array.from(QUEUE_ACTIONS).sort((a, b) => {
-    return new Date(a.date).getTime() - new Date(b.date).getTime();
+  const QUEUE_ACTIONS_SORTED = (incompleteActions || []).sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
   });
 
   return (
@@ -98,23 +64,13 @@ const QueueScreen = () => {
       </Text>
       <View className="flex-1">
         {QUEUE_ACTIONS_SORTED.length > 0 ? (
-          Object.entries(QUEUE_STATUS_CATEGORIES).map(
-            ([status, actions]) =>
-              actions.length > 0 && (
-                <View key={status}>
-                  <Text className="text-base font-dm-medium text-slate-600 tracking-tight mb-1">
-                    {status}
-                  </Text>
-                  <View className="bg-slate-50 rounded-md overflow-hidden mb-6">
-                    <ScrollView>
-                      {actions.map((action) => (
-                        <QueuedAction key={action.title} {...action} />
-                      ))}
-                    </ScrollView>
-                  </View>
-                </View>
-              )
-          )
+          QUEUE_ACTIONS_SORTED.map((action) => (
+            <QueuedAction
+              key={action.localId}
+              type={action.type}
+              date={action.date}
+            />
+          ))
         ) : (
           <View className="flex-1 items-center justify-center">
             <Ionicons name="albums-outline" size={64} color="#475569" />
