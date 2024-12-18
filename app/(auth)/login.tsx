dd4cc45e06e75428f0709b9b54070087f2e3c004
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-18 15:47:17 -0500

    refactor(login): :wheelchair: Improve keyboard avoidance behavior for form and accessibility tags for images

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,73 +1,92 @@
 import LoginForm from "@/components/LoginForm";
-import { useNavigation } from "expo-router";
+import SafeAreaContent from "@/components/SafeAreaContent";
+import { Trans } from "@lingui/react/macro";
 import { AnimatePresence, MotiText } from "moti";
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Image,
   KeyboardAvoidingView,
   Platform,
+  ScrollView,
   Text,
   View,
 } from "react-native";
-import { Trans } from "@lingui/react/macro";
-import SafeAreaContent from "@/components/SafeAreaContent";
+
+const KEYBOARD_OFFSET = Platform.OS === "ios" ? 0 : 500;
+const KEYBOARD_BEHAVIOR = Platform.OS === "ios" ? "padding" : "height";
 
 export default function LoginScreen() {
-  const navigation = useNavigation();
-
-  useEffect(() => {
-    navigation.setOptions({ statusBarStyle: "dark" });
-  }, [navigation]);
-
   return (
     <SafeAreaContent>
       <View className="absolute top-36 right-[-30px] bg-white-sand">
         <Image
           source={require("@/assets/images/animals/Turtle.png")}
           className="w-[223px] h-[228px]"
-        />
-      </View>
-      <View className="mb-8">
-        <Image
-          source={require("@/assets/images/logo-gray.webp")}
-          className="w-[86px] h-[89px] rounded-full"
+          accessibilityLabel="Decorative turtle illustration"
+          accessibilityRole="image"
         />
       </View>
       <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
-        className="flex-1 justify-end"
+        behavior={KEYBOARD_BEHAVIOR}
+        keyboardVerticalOffset={KEYBOARD_OFFSET}
+        className="flex-1"
       >
-        <AnimatePresence presenceAffectsLayout>
-          <MotiText
-            className="text-3xl font-dm-bold"
-            style={{ letterSpacing: -1, lineHeight: 31.35008 }}
-            from={{ opacity: 0, translateY: 10 }}
-            animate={{ opacity: 1, translateY: 0 }}
-            transition={{
-              type: "spring",
-              damping: 20,
-              stiffness: 100,
-              delay: 200,
-            }}
-          >
-            <Trans>Welcome to Enaleia Hub</Trans>
-          </MotiText>
-        </AnimatePresence>
-        <Text className="text-base font-dm-light">
-          Please sign in with the provided credential
-        </Text>
-        <View className="flex-1">
-          <LoginForm />
-        </View>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "space-between",
+          }}
+        >
+          <View className="mb-8 relative">
+            <Image
+              source={require("@/assets/images/logo-gray.webp")}
+              className="w-[86px] h-[89px] rounded-full"
+              accessibilityLabel="Enaleia Hub logo"
+              accessibilityRole="image"
+            />
+          </View>
+          <View className="">
+            <AnimatePresence presenceAffectsLayout>
+              <MotiText
+                className="text-3xl font-dm-bold"
+                style={{ letterSpacing: -1, lineHeight: 31.35008 }}
+                from={{ opacity: 0, translateY: 10 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{
+                  type: "spring",
+                  damping: 20,
+                  stiffness: 100,
+                  delay: 200,
+                }}
+              >
+                <Trans>Welcome to Enaleia Hub</Trans>
+              </MotiText>
+            </AnimatePresence>
+            <Text className="text-base font-dm-light mb-3">
+              Please sign in with the provided credential
+            </Text>
+            <LoginForm />
+            <Text className="text-sm text-grey-8 font-dm-light mt-6 leading-[16.8px]">
+              {/* TODO: Add link to support. example: */}
+              {/* <Link
+                href="/support"
+                className="text-blue-ocean font-dm-bold"
+                accessibilityLabel="Link to support"
+                accessibilityRole="link"
+              >
+                <Trans>Contact support</Trans>
+              </Link> */}
+              <Trans>
+                The Enaleia Hub is an invite-only application designed for
+                ecosystem partners to securely submit data to the blockchain. If
+                you have lost your login information, please click here to
+                contact support.
+              </Trans>
+            </Text>
+          </View>
+        </ScrollView>
       </KeyboardAvoidingView>
-      <Text className="text-sm text-grey-8 font-dm-light">
-        <Trans>
-          The Enaleia Hub is an invite-only application designed for ecosystem
-          partners to securely submit data to the blockchain. If you have lost
-          your login information, please click here to contact support.
-        </Trans>
-      </Text>
     </SafeAreaContent>
   );
 }
