729551f140e9e87140c482165f3ef106822f1533
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-19 12:56:09 -0500

    feat: :lipstick: Improve Scanner UI
    Update close button style, add scan line animation

--- components/QRCodeScanner.tsx
+++ components/QRCodeScanner.tsx
@@ -1,6 +1,13 @@
 import { CameraView, useCameraPermissions } from "expo-camera";
 import React from "react";
-import { Button, Text, View } from "react-native";
+import {
+  Animated,
+  Button,
+  Dimensions,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 
 interface QRCodeScannerProps {
   onScan: (data: string) => void;
@@ -9,6 +16,25 @@ interface QRCodeScannerProps {
 
 const QRCodeScanner: React.FC<QRCodeScannerProps> = ({ onScan, onClose }) => {
   const [permission, requestPermission] = useCameraPermissions();
+  const scanLineAnimation = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    const screenHeight = Dimensions.get("window").height;
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scanLineAnimation, {
+          toValue: screenHeight * 0.7,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scanLineAnimation, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
 
   const handleBarCodeScanned = ({ data }: { data: string }) => {
     if (data) {
@@ -36,13 +62,37 @@ const QRCodeScanner: React.FC<QRCodeScannerProps> = ({ onScan, onClose }) => {
 
   return (
     <View className="flex-1 flex-col justify-center">
-      <CameraView
-        facing={"back"}
-        className="flex-1"
-        onBarcodeScanned={handleBarCodeScanned}
-      />
+      <Text className="text-center p-4 text-sm font-dm-bold text-enaleia-black tracking-tighter bg-white-sand">
+        Point the camera at a QR code
+      </Text>
+
+      <View className="flex-1 relative">
+        <CameraView
+          facing={"back"}
+          className="flex-1"
+          onBarcodeScanned={handleBarCodeScanned}
+        />
+        <Animated.View
+          style={{
+            transform: [{ translateY: scanLineAnimation }],
+            position: "absolute",
+            left: 0,
+            right: 0,
+            height: 2,
+            backgroundColor: "#2985D0",
+            opacity: 0.7,
+          }}
+        />
+      </View>
       <View className="absolute bottom-10 left-0 right-0 items-center">
-        <Button title="Close Scanner" onPress={onClose} />
+        <Pressable
+          className="bg-blue-ocean min-w-[60px] px-3 py-2 rounded-3xl flex flex-row items-center justify-center mx-1 my-1"
+          onPress={onClose}
+        >
+          <Text className="text-sm font-dm-bold text-white tracking-tighter">
+            Close scanner
+          </Text>
+        </Pressable>
       </View>
     </View>
   );
