d61dc19a3d69c4814e1f3864aa22e32c07ca390b
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-30 16:59:41 -0500

    feat: :sparkles: Add ModalBase component

---
+++ components/modals/ModalBase.tsx
@@ -0,0 +1,57 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import {
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  Pressable,
+  ScrollView,
+  View,
+} from "react-native";
+
+export default function ModalBase({
+  isVisible,
+  onClose,
+  children,
+  canClose = true,
+}: {
+  isVisible: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  canClose?: boolean;
+}) {
+  return (
+    <Modal
+      visible={isVisible}
+      onRequestClose={onClose}
+      transparent={true}
+      animationType="fade"
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex-1 bg-slate-950/75 p-3"
+      >
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "center",
+          }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View className="bg-slate-50 p-3 rounded-lg relative">
+            {children}
+
+            {canClose && (
+              <Pressable
+                onPress={onClose}
+                className="h-10 w-10 absolute right-0 top-3"
+              >
+                <Ionicons name="close" size={20} color="gray" />
+              </Pressable>
+            )}
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </Modal>
+  );
+}
