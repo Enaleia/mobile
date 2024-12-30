c4315662a5f42e902e7e1545f7a782ba0b8b2d96
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-12-30 17:33:27 -0500

    feat: :sparkles: Add SentToQueue modal

---
+++ components/modals/SentToQueueModal.tsx
@@ -0,0 +1,54 @@
+import ModalBase from "@/components/modals/ModalBase";
+import { router } from "expo-router";
+import { View } from "moti";
+import { Image, Pressable, Text } from "react-native";
+
+export const SentToQueueModal = ({
+  isVisible,
+  onClose,
+}: {
+  isVisible: boolean;
+  onClose: () => void;
+}) => {
+  return (
+    <ModalBase isVisible={isVisible} onClose={onClose} canClose={false}>
+      <View className="flex-row justify-center items-center py-4">
+        <Image
+          source={require("@/assets/images/animals/Fish.png")}
+          className="w-[133px] h-[134px]"
+        />
+      </View>
+      <View className="space-y-2">
+        {/* TODO:This should probably be reworded tbh */}
+        <Text className="text-xl font-dm-bold text-enaleia-black tracking-tighter text-center">
+          Attestation queued successfully
+        </Text>
+        <Text className="text-base font-dm-regular text-enaleia-black tracking-tighter text-center">
+          Your attestation was sent to the queue for submission.
+        </Text>
+        <Text className="text-sm font-dm-regular text-enaleia-black tracking-tighter text-center">
+          Important: If working offline, please make sure to get online
+          periodically to complete the submission(s).
+        </Text>
+      </View>
+      <View className="flex-row justify-center items-center py-4 gap-2">
+        <Pressable
+          onPress={() => router.replace("/queue")}
+          className="border border-blue-ocean rounded-full bg-slate-50 p-3 flex-1"
+        >
+          <Text className="text-blue-ocean font-dm-regular text-base text-center">
+            See queue
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() => router.replace("/home")}
+          className="bg-blue-ocean rounded-full p-3 flex-1"
+        >
+          <Text className="text-white font-dm-regular text-base text-center">
+            Go home
+          </Text>
+        </Pressable>
+      </View>
+    </ModalBase>
+  );
+};
