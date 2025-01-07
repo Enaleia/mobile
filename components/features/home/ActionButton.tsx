2da1f26f74936691150bd0d9b10da29d491d0f5c
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-01-07 18:44:04 -0500

    refactor: :lipstick: Update action button and action select based on data retreival changes

--- components/features/home/ActionButton.tsx
+++ components/features/home/ActionButton.tsx
@@ -1,7 +1,6 @@
-import { ACTION_COLORS, ACTION_ICONS, ACTION_SLUGS } from "@/constants/action";
-import { ActionTitle } from "@/types/action";
+import { ACTION_SLUGS } from "@/constants/action";
 import { router } from "expo-router";
-import { Image, Pressable, Text } from "react-native";
+import { Image, ImageSourcePropType, Pressable, Text } from "react-native";
 
 const buttonClassName = "items-center justify-center px-2 py-3 rounded-2xl";
 
@@ -9,25 +8,31 @@ const buttonClasses = `w-[48%] mr-1 mb-1 active:opacity-70 transition-opacity du
 const bannerClasses = `w-full ${buttonClassName}`;
 
 const ActionButton = ({
-  title,
   presentation = "button",
+  name,
+  color,
+  icon,
 }: {
-  title: ActionTitle;
+  name: string;
+  color: string;
+  icon: ImageSourcePropType;
   presentation?: "button" | "banner";
 }) => {
   return (
     <Pressable
       className={presentation === "button" ? buttonClasses : bannerClasses}
-      style={{ backgroundColor: ACTION_COLORS[title] }}
+      style={{ backgroundColor: color }}
       onPress={() =>
         presentation === "button"
-          ? router.push(`/attest/new/${ACTION_SLUGS[title]}`)
+          ? router.push(
+              `/attest/new/${ACTION_SLUGS[name as keyof typeof ACTION_SLUGS]}`
+            )
           : null
       }
     >
-      <Image source={ACTION_ICONS[title]} className="w-16 h-16" />
+      <Image source={icon} className="w-16 h-16" />
       <Text className="text-sm font-dm-light tracking-[-0.25px] text-enaleia-black text-center">
-        {title}
+        {name}
       </Text>
     </Pressable>
   );
--- components/features/home/ActionSelect.tsx
+++ components/features/home/ActionSelect.tsx
@@ -1,30 +1,32 @@
-import { ACTION_CATEGORIES } from "@/constants/action";
-import { ScrollView, Text, View } from "react-native";
 import ActionButton from "@/components/features/home/ActionButton";
-
-import { useQuery } from "@tanstack/react-query";
-import { fetchActions } from "@/services/directus";
+import { ScrollView, Text, View } from "react-native";
 import { LoadingScreen } from "@/components/LoadingScreen";
+import { NoDataScreen } from "@/components/NoDataScreen";
+import { Action } from "@/types/action";
+import { GroupedActions } from "@/hooks/useActions";
 
-// Custom hook to fetch and manage actions data
-function useActions() {
-  return useQuery({
-    queryKey: ["actions"],
-    queryFn: async () => {
-      const actions = await fetchActions();
-      return actions;
-    },
-  });
+interface ActionSelectionProps {
+  actions: GroupedActions | undefined;
+  isLoading: boolean;
 }
 
-export default function ActionSelection() {
-  const { data: actions, isLoading } = useActions();
+export default function ActionSelection({
+  actions,
+  isLoading,
+}: ActionSelectionProps) {
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
 
-  if (isLoading) return <LoadingScreen />;
+  if (!actions) {
+    return (
+      <NoDataScreen message="Could not load actions, check your internet connection" />
+    );
+  }
 
   return (
     <ScrollView>
-      {Object.entries(ACTION_CATEGORIES).map(([category, actions]) => (
+      {Object.entries(actions).map(([category, categoryActions]) => (
         <View
           key={category}
           className="py-5 border-b-[1.5px] border-neutral-200 last-of-type:border-b-0"
@@ -33,8 +35,13 @@ export default function ActionSelection() {
             {category}
           </Text>
           <View className="flex-row flex-wrap">
-            {actions.map((action) => (
-              <ActionButton key={action} title={action} />
+            {categoryActions.map((action: Action) => (
+              <ActionButton
+                key={action.id}
+                name={action.name}
+                color={action.color}
+                icon={action.icon}
+              />
             ))}
           </View>
         </View>
