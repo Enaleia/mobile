f139561e33d762f34d71b61afa11db742090ae2e
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-01-06 20:56:52 -0500

    refactor: :card_file_box: Fetch actions from stored data

--- components/features/home/ActionSelect.tsx
+++ components/features/home/ActionSelect.tsx
@@ -2,7 +2,26 @@ import { ACTION_CATEGORIES } from "@/constants/action";
 import { ScrollView, Text, View } from "react-native";
 import ActionButton from "@/components/features/home/ActionButton";
 
+import { useQuery } from "@tanstack/react-query";
+import { fetchActions } from "@/services/directus";
+import { LoadingScreen } from "@/components/LoadingScreen";
+
+// Custom hook to fetch and manage actions data
+function useActions() {
+  return useQuery({
+    queryKey: ["actions"],
+    queryFn: async () => {
+      const actions = await fetchActions();
+      return actions;
+    },
+  });
+}
+
 export default function ActionSelection() {
+  const { data: actions, isLoading } = useActions();
+
+  if (isLoading) return <LoadingScreen />;
+
   return (
     <ScrollView>
       {Object.entries(ACTION_CATEGORIES).map(([category, actions]) => (
