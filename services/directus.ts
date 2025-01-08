1dc1be294735d95abf5b35d83e3c9ff441e1cbed
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-01-08 16:34:06 -0500

    refactor: :recycle: Refactor fetch functions in the directus service

--- services/directus.ts
+++ services/directus.ts
@@ -1,37 +1,18 @@
-import { ActionTitle } from "@/types/action";
 import { directus } from "@/utils/directus";
 import { readItems } from "@directus/sdk";
 
-export interface DirectusAction {
-  id: number;
-  name: ActionTitle;
-  slug: string;
-  color: string;
-  icon: string;
-  category: string;
-}
-
-export interface DirectusMaterial {
-  id: number;
-  name: string;
-}
-
 export async function fetchMaterials() {
-  try {
-    const materials = await directus.request(readItems("Materials"));
-    return materials;
-  } catch (error) {
-    console.error("Error fetching materials:", error);
-    throw error;
-  }
+  return directus.request(readItems("Materials"));
 }
 
 export async function fetchActions() {
-  try {
-    const actions = await directus.request(readItems("Actions"));
-    return actions;
-  } catch (error) {
-    console.error("Error fetching actions:", error);
-    throw error;
-  }
+  return directus.request(readItems("Actions"));
+}
+
+export async function fetchCollectors() {
+  return directus.request(readItems("Collectors"));
+}
+
+export async function fetchProducts() {
+  return directus.request(readItems("Products"));
 }
