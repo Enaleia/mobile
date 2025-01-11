6a67d16a2c473c68f272cea23ca58cd9967ab524
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-01-11 16:53:39 -0500

    chore: :wrench: Update sourceExts in metro config

--- metro.config.js
+++ metro.config.js
@@ -10,7 +10,7 @@ config.transformer = {
 };
 config.resolver = {
   ...resolver,
-  sourceExts: [...resolver.sourceExts, "po", "pot"],
+	sourceExts: [...resolver.sourceExts],
 };
 
 module.exports = config;
