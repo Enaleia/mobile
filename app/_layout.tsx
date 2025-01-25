89e5c0195357af6f3b19538a35ade69c6cc7a23a
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-01-25 15:14:03 -0500

    fix(route): :speech_balloon: Change attestation route name in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -112,7 +112,7 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
           <Stack.Screen
-            name="attest/new/[type]"
+            name="attest/new/[slug]"
             options={{ headerShown: false }}
           />
         </Stack>
