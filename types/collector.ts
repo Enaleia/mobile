a2aea52025ac59aae3647fbb2b9cd57da47b1a17
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-02-04 12:43:33 -0500

    chore(types): :label: Add comments to collector type

--- types/collector.ts
+++ types/collector.ts
@@ -15,23 +15,82 @@ export const processCollectors = (collectors: any[]): Collector[] => {
 };
 
 export interface DirectusCollector {
+  /**
+   * The ID of the collector
+   */
   collector_id: number;
+  /**
+   * The status of the collector
+   */
   status: "published" | "draft" | "archived";
+  /**
+   * The sort order of the collector
+   */
   sort?: number;
+  /**
+   * The user who created the collector
+   */
   user_created?: string; // UUID
+  /**
+   * The date the collector was created
+   */
   date_created?: string;
+  /**
+   * The user who updated the collector
+   */
   user_updated?: string; // UUID
+  /**
+   * The date the collector was updated
+   */
   date_updated?: string;
+  /**
+   * Whether the collector is active
+   */
   is_active?: boolean;
+  /**
+   * The contact person of the collector
+   */
   contact_person?: string;
-  fishing_season?: JsonValue; // json type
-  country?: number; // Foreign key to countries
+  /**
+   * The fishing season of the collector
+   */
+  fishing_season?: JsonValue;
+  /**
+   * The country of the collector
+   *  foreign key to countries
+   */
+  country?: number;
+  /**
+   * The name of the collector company
+   */
   collector_company_name?: string;
-  vessel_type?: number; // Foreign key to Vessels_Type
-  registered_port?: number; // Foreign key to Companies
-  fishing_zone?: any; // geometry.Polygon type
+  /**
+   * The type of vessel of the collector
+   */
+  vessel_type?: number;
+  /**
+   * The registered port of the collector
+   *  foreign key to Companies
+   */
+  registered_port?: number;
+  /**
+   * The fishing zone of the collector
+   */
+  fishing_zone?: any;
+  /**
+   * The scanned QR code from the collector's card
+   */
   collector_identity?: string;
+  /**
+   * The name of the collector
+   */
   collector_name?: string;
+  /**
+   * The start date of the collector
+   */
   start_date?: string;
-  events?: any; // o2m relationship alias
+  /**
+   * The events of the collector
+   */
+  events?: any;
 }
