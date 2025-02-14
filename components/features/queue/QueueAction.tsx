9256da5bb00b3e16041ceabd0fd9c26436336b27
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-02-14 16:54:50 -0500

    feat(queue): :alien: use action data from API in QueueAction

--- components/features/queue/QueueAction.tsx
+++ components/features/queue/QueueAction.tsx
@@ -1,15 +1,21 @@
 import { ACTION_ICONS } from "@/constants/action";
-import { ActionTitle } from "@/types/action";
+import { useActions } from "@/hooks/data/useActions";
+import { QueueItem } from "@/types/queue";
 import { View, Text, Image } from "react-native";
 
-const QueuedAction = ({ type, date }: { type: ActionTitle; date: string }) => {
+const QueuedAction = ({ item }: { item: QueueItem }) => {
+  const { actionsData } = useActions();
+  const action = actionsData?.find((a) => a.id === item.actionId);
+
+  if (!action) return null;
+
   return (
     <View className="flex-row items-center justify-between px-3 py-2 border-b border-neutral-200">
       <View className="flex-row items-center justify-center gap-2">
-        <Image source={ACTION_ICONS[type]} className="w-8 h-8" />
+        <Image source={ACTION_ICONS[action.name]} className="w-8 h-8" />
         <View className="space-y-0.5">
           <Text className="text-base font-dm-bold text-slate-800 tracking-tighter">
-            {type}
+            {action.name}
           </Text>
           <Text className="text-xs font-dm-medium text-slate-500 uppercase w-full">
             {new Intl.DateTimeFormat("en-US", {
@@ -18,7 +24,7 @@ const QueuedAction = ({ type, date }: { type: ActionTitle; date: string }) => {
               year: "numeric",
               hour: "numeric",
               minute: "numeric",
-            }).format(new Date(date))}
+            }).format(new Date(item.date))}
           </Text>
         </View>
       </View>
