8f8c13d49e56e6d530e55e7121e186cf8efa9020
Chiali <info@chialitsai.com>
2025-03-06 14:06:59 -0500

    touch up
    - modify input field height, login error msg, remove offline mode message on login page
    - add user name to queue page
    - add queue page title
    - make queue and setting full page scroll

--- components/shared/ErrorMessage.tsx
+++ components/shared/ErrorMessage.tsx
@@ -1,4 +1,3 @@
-import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { View, Text } from "react-native";
 
@@ -14,34 +13,35 @@ interface ErrorMessageProps {
   secondaryMessage?: string;
 
   /**
+   * Whether to hide the secondary message
+   */
+  hideSecondaryMessage?: boolean;
+
+  /**
    * Optional additional class names for the container
    */
   className?: string;
 }
 
 /**
- * A reusable error message component that displays an error with an icon
+ * A reusable error message component that displays an error message
  */
 const ErrorMessage: React.FC<ErrorMessageProps> = ({
   message,
   secondaryMessage = "Please try again or contact support if the problem persists.",
+  hideSecondaryMessage = false,
   className = "",
 }) => {
   return (
     <View
-      className={`flex-row items-center bg-red-50 border border-red-200 rounded-lg p-3 ${className}`}
+      className={`bg-red-50 border border-red-200 rounded-lg p-3 ${className}`}
     >
-      <View className="bg-red-100 rounded-full p-1 mr-2">
-        <Ionicons name="alert-circle" size={20} color="#EF4444" />
-      </View>
-      <View className="flex-1">
-        <Text className="text-sm font-dm-medium text-red-700">{message}</Text>
-        {secondaryMessage && (
-          <Text className="text-xs font-dm-regular text-red-600 mt-1">
-            {secondaryMessage}
-          </Text>
-        )}
-      </View>
+      <Text className="text-sm font-dm-medium text-red-700">{message}</Text>
+      {!hideSecondaryMessage && secondaryMessage && (
+        <Text className="text-xs font-dm-regular text-red-600 mt-1">
+          {secondaryMessage}
+        </Text>
+      )}
     </View>
   );
 };
