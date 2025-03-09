104c98658c316e4d00bbf7bba0472b60b66c0e24
Chiali <info@chialitsai.com>
2025-03-09 16:32:09 -0400

    add sentry
    - add sentry
    - add empty state in. queue page
    - add contact support on setting

--- app/attest/new/[slug].tsx
+++ app/attest/new/[slug].tsx
@@ -407,7 +407,9 @@ const NewActionScreen = () => {
         isVisible={isTypeInformationModalVisible}
         onClose={() => setIsTypeInformationModalVisible(false)}
       />
-
+      <Text className="text-3xl font-dm-bold text-enaleia-black tracking-[-1px] mb-2">
+        {currentAction?.name}
+      </Text>
       <View className="flex-1">
         <ScrollView
           ref={scrollViewRef}
@@ -418,10 +420,7 @@ const NewActionScreen = () => {
             flexGrow: 0,
             paddingBottom: 100,
           }}
-        >      
-      <Text className="text-3xl font-dm-bold text-enaleia-black tracking-[-1px] mb-2">
-        {currentAction?.name}
-      </Text>
+        >
           <View className="flex-1">
             <form.Subscribe selector={(state) => state.values}>
               {(values) => (
@@ -444,7 +443,7 @@ const NewActionScreen = () => {
             {currentAction?.category === "Collection" && (
               <View className="mb-8">
                 <Text className="text-[18px] font-dm-regular text-enaleia-black tracking-tighter mb-2">
-                  Collector 
+                  Collector
                 </Text>
                 <form.Field name="collectorId">
                   {(field) => (
@@ -453,7 +452,7 @@ const NewActionScreen = () => {
                         value={field.state.value || ""}
                         onChangeText={field.handleChange}
                         variant="standalone"
-                        label="Collector ID Card (Required)"
+                        label="Collector ID Card"
                         error={field.state.meta.errors?.[0] || undefined}
                       />
                     </>
@@ -541,7 +540,7 @@ const NewActionScreen = () => {
                               placeholder="0"
                               allowDecimals={false}
                               suffix="Unit"
-                              error={typeof field.state.meta.errors?.[0] === 'string' ? field.state.meta.errors[0] : undefined}
+                              error={field.state.meta.errors?.[0]}
                             />
                           );
                           return <QuantityField />;
@@ -558,7 +557,7 @@ const NewActionScreen = () => {
                               label="Weight per item"
                               placeholder="0"
                               suffix="kg"
-                              error={typeof field.state.meta.errors?.[0] === 'string' ? field.state.meta.errors[0] : undefined}
+                              error={field.state.meta.errors?.[0] || undefined}
                             />
                           );
                           return <WeightField />;
@@ -573,9 +572,7 @@ const NewActionScreen = () => {
         </ScrollView>
 
         {/* Fixed Submit Button */}
-
-      </View>
-      <View className="absolute bottom-[0px] left-0 right-0 bg-white px-5 pt-2 pb-9">
+        <View className="absolute bottom-[48px] left-0 right-0 bg-white-sand px-5">
           <form.Subscribe
             selector={(state) => [
               state.canSubmit,
@@ -656,6 +653,7 @@ const NewActionScreen = () => {
             }}
           </form.Subscribe>
         </View>
+      </View>
       {isSentToQueue && (
         <SentToQueueModal isVisible={isSentToQueue} onClose={() => {}} />
       )}
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,8 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
 
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 const { transformer, resolver } = config;
 
 config.transformer = {
