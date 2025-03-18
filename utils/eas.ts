08057dc52b89545379e447a8e3f58a5ac9d4b599
kinizumi <kinizumi@gmail.com>
2025-03-18 14:21:07 -0400

    fixes company & code
    fixes company & code

--- utils/eas.ts
+++ utils/eas.ts
@@ -79,8 +79,7 @@ export const mapToEASSchema = (
       ) || [],
     incomingWeightsKg,
     incomingCodes: [
-      ...(form.incomingMaterials?.map((m) => m.code || "") || []),
-      form.collectorId || "",
+      ...(form.incomingMaterials?.map((m) => m.code ||form.collectorId || "") || [])
     ].filter(Boolean),
 
     outgoingMaterials:
@@ -118,7 +117,8 @@ export const validateEASSchema = (data: EnaleiaEASSchema): boolean => {
 
   if (
     data.incomingMaterials.length > 0 &&
-    (data.incomingMaterials.length !== data.incomingWeightsKg.length)
+    (data.incomingMaterials.length !== data.incomingWeightsKg.length||
+      data.incomingMaterials.length !== data.incomingCodes.length)
   ) {
     throw new Error(
       "Incoming materials, weights, and codes must have matching lengths"
