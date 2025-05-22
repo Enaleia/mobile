4ae48c54fa8464a632db52a8bb2cf4348378e2d1
Chiali <info@chialitsai.com>
2025-05-22 15:02:49 -0400

    update to mainnet and minor package update

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = function (api) {
     plugins: [
       "@lingui/babel-plugin-lingui-macro",
       "nativewind/babel",
-      "@babel/plugin-proposal-export-namespace-from",
+      "@babel/plugin-transform-export-namespace-from",
       "react-native-reanimated/plugin",
       "@babel/plugin-transform-runtime"
     ],
